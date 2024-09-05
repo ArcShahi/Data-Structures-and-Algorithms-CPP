0a9e958f0ddf3c83e60226335e78540fc1be78f4
ArcShahi <arcshahi54@gmail.com>
2024-09-05 23:12:11 +0530

    Updated Methods

--- Data_Structures/singly_linked_list.hpp
+++ Data_Structures/singly_linked_list.hpp
@@ -2,7 +2,7 @@
 #define LINKED_LIST_HPP
 
 #include <memory>
-#include <stdexcept>
+
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -31,11 +31,13 @@ public:
 
 	size_t ssize();
 
-	type back()const;
+	type& back()const;
 
-	type front()const;
+	type& front()const;
 
 	void clear();
+
+	void yeet(type value);
 
 	// void erase(type value );
 
@@ -121,8 +123,10 @@ void Linked_List<type>::pop_back()
 {
 	if (isEmpty())
 	{
-		throw std::out_of_range("ERROR: Attempt to call 'pop_back() on empty list");
+		std::cerr << "ERROR: Attempt to call 'pop_back() on empty list\n";
+		return;
 	}
+
 	if (head == tail)
 	{
 		head = tail = nullptr;
@@ -148,7 +152,8 @@ void Linked_List<type>::pop_front()
 {
 	if (isEmpty())
 	{
-		throw std::out_of_range("ERROR: Attempt to call 'pop_front() on empty list");
+		std::cerr << "ERROR: Attempt to call 'pop_front() on empty list\n";
+		return;
 	}
 
 	// Set Head Pointer to Point at Second Node
@@ -186,22 +191,23 @@ size_t Linked_List<type>::ssize()
 }
 
 template<typename type>
-type Linked_List<type>::back() const
+type& Linked_List<type>::back() const
 {
 	if (isEmpty())
 	{
-		throw std::out_of_range("ERROR: Attempt to access 'back()' on empty List\n");
-
+		return tail->next->data;
 	}
+
 	return tail->data;
 }
 
 template<typename type>
-type Linked_List<type>::front() const
+type& Linked_List<type>::front() const
 {
+
 	if (isEmpty())
 	{
-		throw std::out_of_range("ERROR: Attempt to access 'front()' on empty List\n");
+		return tail->next->data;
 	}
 	return head->data;
 }
@@ -214,13 +220,61 @@ void Linked_List<type>::clear()
 	size = 0;
 }
 
+// Removes the First occurence of Value
+template<typename type>
+void Linked_List<type>::yeet(type value)
+{
+	if (isEmpty())
+	{
+		std::cerr << "LIST IS EMPTY\n";
+		return;
+
+	}
+
+	// If  the value is in the head node
+	if (value == head->data)
+	{
+		pop_front();
+		return;
+	}
+
+	// If the value is in the tail node
+	if (value == tail->data)
+	{
+		pop_back(); // Use the existing pop_back() method
+		return;
+	}
+
+
+
+	auto itr = head;
+
+	// Find the the Previous Node of the Element to Remove ( Making sure it's Not null )
+	while (itr->next != nullptr && itr->next->data != value)
+	{
+		itr = itr->next;
+	}
+	// Now, itr->next points to the node that needs to be removed
+	if (itr->next == nullptr)
+	{
+		std::cerr << value << " Not Found in List\n";
+		return;
+	}
+	// Remove the node by linking to the next one
+	itr->next = itr->next->next;
+
+
+	--size;
+}
+
 // Checks Wheter an Value exists in List
 template<typename type>
 bool Linked_List<type>::search(const type value)const
 {
 	if (isEmpty())
 	{
-		throw std::out_of_range("ERROR: Attemp to call 'search()' on empty List");
+
+		return false;
 	}
 
 	for (auto itr = head; itr != nullptr; itr = itr->next)
@@ -238,7 +292,8 @@ void Linked_List<type>::replace(const type value, const type newValue)
 {
 	if (isEmpty())
 	{
-		throw std::out_of_range("ERROR: Attemp to call 'replace()' on empty List");
+		std::cout << "ERROR: Attemp to call 'replace()' on empty List\n";
+		return;
 	}
 
 	for (auto itr = head; itr != nullptr; itr = itr->next)
