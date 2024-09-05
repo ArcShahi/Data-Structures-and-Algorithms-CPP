fd5ee097cbfa56b263db733f1ac06b3b2aecac3f
ArcShahi <arcshahi54@gmail.com>
2024-09-05 12:23:00 +0530

    Added new Methods

--- Data_Structures/singly_linked_list.hpp
+++ Data_Structures/singly_linked_list.hpp
@@ -50,6 +50,54 @@ public:
 		}
 		++size;
 	}
+
+
+	void pop_back()
+	{
+		if (isEmpty())
+		{
+			throw std::out_of_range("ERROR: Attempt to call 'pop_back() on empty list");
+		}
+		if (head == tail)
+		{
+			head = tail = nullptr;
+		}
+		else
+		{
+			auto current = head;
+			// Traverse to Second Last Node
+			while (current->next != tail) {
+				current = current->next;
+			}
+			// Remove current-> next ( last element )
+			current->next = nullptr;
+
+			// Set Pointer Tail to Current ( Second last element )
+			tail = current;
+		}
+		--size;
+	}
+
+	void pop_front()
+	{
+		if (isEmpty())
+		{
+			throw std::out_of_range("ERROR: Attempt to call 'pop_front() on empty list");
+		}
+
+		// Set Head Pointer to Point at Second Node
+		head = head->next;
+
+
+
+		if (head == nullptr)
+		{
+			tail = nullptr;
+		}
+
+		--size;
+	}
+
 	void display() const
 	{
 		if (isEmpty())
@@ -57,9 +105,9 @@ public:
 			std::cout << "EMPTY LIST\n";
 			return;
 		}
-		for (auto current = head; current != nullptr; current = current->next)
+		for (auto itr = head; itr != nullptr; itr = itr->next)
 		{
-			std::cout << current->data << ' ';
+			std::cout << itr->data << ' ';
 		}
 		std::cout << '\n';
 	}
@@ -86,7 +134,7 @@ public:
 
 		if (isEmpty())
 		{
-			throw std::out_of_range("ERROR: Attempt to access 'back()' on empty List\n");
+			throw std::out_of_range("ERROR: Attempt to access 'front()' on empty List\n");
 		}
 		return head->data;
 
@@ -98,6 +146,70 @@ public:
 		head = tail = nullptr;
 		size = 0;
 	}
+
+	// Checks Wheter an Value exists in List
+	bool search(type value)
+	{
+		if (isEmpty())
+		{
+			throw std::out_of_range("ERROR: Attemp to call 'search()' on empty List");
+		}
+
+		for (auto itr = head; itr != nullptr; itr = itr->next)
+		{
+			if (itr->data == value)
+			{
+				return true;
+			}
+		}
+		return false;
+
+
+	}
+
+	void replace(const type value, const type newValue)
+	{
+
+		if (isEmpty())
+		{
+			throw std::out_of_range("ERROR: Attemp to call 'replace()' on empty List");
+		}
+
+		for (auto itr = head; itr != nullptr; itr = itr->next)
+		{
+			if (itr->data == value)
+			{
+				itr->data = newValue;
+				break;
+			}
+		}
+
+	}
+
+	// TODO - Might make it const pointer type 
+	std::shared_ptr<type>  find(const type value)
+
+	{
+
+
+		if (isEmpty())
+		{
+			throw std::out_of_range("ERROR: Attemp to call 'find()' on empty List");
+		}
+
+
+		for (auto itr = head; itr != nullptr; itr = itr->next)
+		{
+			if (itr->data == value)
+			{
+				// Returned a Shared Pointer to itr
+				return std::make_shared<type>(itr->data);
+			}
+		}
+		return nullptr; // Element not found
+	}
+
+
 private:
 
 	struct  Node
