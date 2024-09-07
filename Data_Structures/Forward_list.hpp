3799ef2989cf864db3511768353e358611a351a8
ArcShahi <arcshahi54@gmail.com>
2024-09-07 16:21:39 +0530

    Moved class inside namespace

--- Data_Structures/Forward_list.hpp
+++ Data_Structures/Forward_list.hpp
@@ -1,5 +1,5 @@
-#ifndef LINKED_LIST_HPP
-#define LINKED_LIST_HPP
+#ifndef FORWARD_LIST_HPP
+#define FORWARD_LIST_HPP
 
 #include <memory>
 
@@ -10,321 +10,325 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-template<typename type>
-class Linked_List
+namespace xe
 {
-public:
-	Linked_List() {}
 
-	bool isEmpty() const;
-
-
-	void push_back(type value);
-
-	void push_front(type value);
-
-	void pop_back();
-
-	void pop_front();
-
-	void display() const;
-
-	size_t ssize();
-
-	type& back()const;
-
-	type& front()const;
-
-	void clear();
-
-	void yeet(type value);
-
-	void reverse();
-
-	bool search(const type value)const;
-
-	void replace(const type value, const type newValue);
-
-
-
-private:
-
-	struct  Node
+	template<typename type>
+	class Linked_List
 	{
-		explicit Node(type value) :data{ value }, next{ nullptr } {}
-		type data{};
-		std::shared_ptr<Node> next{};
+	public:
+		Linked_List() {}
+
+		bool isEmpty() const;
+
+
+		void push_back(type value);
+
+		void push_front(type value);
+
+		void pop_back();
+
+		void pop_front();
+
+		void display() const;
+
+		size_t ssize();
+
+		type& back()const;
+
+		type& front()const;
+
+		void clear();
+
+		void yeet(type value);
+
+		void reverse();
+
+		bool search(const type value)const;
+
+		void replace(const type value, const type newValue);
+
+
+
+	private:
+
+		struct  Node
+		{
+			explicit Node(type value) :data{ value }, next{ nullptr } {}
+			type data{};
+			std::shared_ptr<Node> next{};
+		};
+
+	private:
+
+		// Initalizes both Head & tail with nullptr.
+		std::shared_ptr<Node> head{};
+		std::shared_ptr<Node> tail{};
+
+		size_t size{};
 	};
 
-private:
-
-	// Initalizes both Head & tail with nullptr.
-	std::shared_ptr<Node> head{};
-	std::shared_ptr<Node> tail{};
-
-	size_t size{};
-};
 
 
 
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-///						TEMPLATE DEFINITION										///
-/////////////////////////////////////////////////////////////////////////////////////////////////
-
+	///////////////////////////////////////////////////////////////////////////////////////////////////
+	///						TEMPLATE DEFINITION										///
+	/////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-template<typename type>
-bool Linked_List<type>::isEmpty() const
-{
-	return head == nullptr;
-}
 
-template<typename type>
-void Linked_List<type>::push_back(type value)
-{
-	auto newNode = std::make_shared<Node>(value);
-	if (isEmpty())
+	template<typename type>
+	bool Linked_List<type>::isEmpty() const
 	{
-		head = newNode;
-		tail = newNode;
-	}
-	else
-	{
-		tail->next = newNode;
-		tail = newNode;
-	}
-	++size;
-}
-
-template<typename type>
-void Linked_List<type>::push_front(type value)
-{
-	auto newNode = std::make_shared<Node>(value);
-
-	if (isEmpty())
-	{
-		head = newNode;
-		tail = newNode;
-	}
-	else
-	{
-		// newNode should point to Head ( First Element in List )
-		newNode->next = head;
-		// Set Head to newNode ( Now the First Element in List)
-		head = newNode;
-	}
-	++size;
-}
-
-template<typename type>
-void Linked_List<type>::pop_back()
-{
-	if (isEmpty())
-	{
-		std::cerr << "ERROR: Attempt to call 'pop_back() on empty list\n";
-		return;
+		return head == nullptr;
 	}
 
-	if (head == tail)
+	template<typename type>
+	void Linked_List<type>::push_back(type value)
 	{
-		head = tail = nullptr;
-	}
-	else
-	{
-		auto current = head;
-		// Traverse to Second Last Node
-		while (current->next != tail) {
-			current = current->next;
+		auto newNode = std::make_shared<Node>(value);
+		if (isEmpty())
+		{
+			head = newNode;
+			tail = newNode;
 		}
-		// Remove current-> next ( last element )
-		current->next = nullptr;
-
-		// Set Pointer Tail to Current ( Second last element )
-		tail = current;
-	}
-	--size;
-}
-
-template<typename type>
-void Linked_List<type>::pop_front()
-{
-	if (isEmpty())
-	{
-		std::cerr << "ERROR: Attempt to call 'pop_front() on empty list\n";
-		return;
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		++size;
 	}
 
-	// Set Head Pointer to Point at Second Node
-	head = head->next;
-
-
-
-	if (head == nullptr)
+	template<typename type>
+	void Linked_List<type>::push_front(type value)
 	{
-		tail = nullptr;
+		auto newNode = std::make_shared<Node>(value);
+
+		if (isEmpty())
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			// newNode should point to Head ( First Element in List )
+			newNode->next = head;
+			// Set Head to newNode ( Now the First Element in List)
+			head = newNode;
+		}
+		++size;
 	}
 
-	--size;
-}
-
-template<typename type>
-void Linked_List<type>::display() const
-{
-	if (isEmpty())
+	template<typename type>
+	void Linked_List<type>::pop_back()
 	{
-		std::cout << "EMPTY LIST\n";
-		return;
-	}
-	for (auto itr = head; itr != nullptr; itr = itr->next)
-	{
-		std::cout << itr->data << "->";
-	}
-	std::cout << '\n';
-}
+		if (isEmpty())
+		{
+			std::cerr << "ERROR: Attempt to call 'pop_back() on empty list\n";
+			return;
+		}
 
-template<typename type>
-size_t Linked_List<type>::ssize()
-{
-	return size;
-}
+		if (head == tail)
+		{
+			head = tail = nullptr;
+		}
+		else
+		{
+			auto current = head;
+			// Traverse to Second Last Node
+			while (current->next != tail) {
+				current = current->next;
+			}
+			// Remove current-> next ( last element )
+			current->next = nullptr;
 
-template<typename type>
-type& Linked_List<type>::back() const
-{
-	return tail->data;
-}
-
-// Invokes UB when called on Empty list
-template<typename type>
-type& Linked_List<type>::front() const
-{
-	return head->data;
-}
-
-// Invokes UB when called on Empty list
-template<typename type>
-void Linked_List<type>::clear()
-{
-	// set Head and Tail to nullptr and reset size
-	head = tail = nullptr;
-	size = 0;
-}
-
-// Removes the First occurence of Value
-template<typename type>
-void Linked_List<type>::yeet(type value)
-{
-	if (isEmpty())
-	{
-		std::cerr << "LIST IS EMPTY\n";
-		return;
-
+			// Set Pointer Tail to Current ( Second last element )
+			tail = current;
+		}
+		--size;
 	}
 
-	// If  the value is in the head node
-	if (value == head->data)
+	template<typename type>
+	void Linked_List<type>::pop_front()
 	{
-		pop_front();
-		return;
+		if (isEmpty())
+		{
+			std::cerr << "ERROR: Attempt to call 'pop_front() on empty list\n";
+			return;
+		}
+
+		// Set Head Pointer to Point at Second Node
+		head = head->next;
+
+
+
+		if (head == nullptr)
+		{
+			tail = nullptr;
+		}
+
+		--size;
 	}
 
-	// If the value is in the tail node
-	if (value == tail->data)
+	template<typename type>
+	void Linked_List<type>::display() const
 	{
-		pop_back(); // Use the existing pop_back() method
-		return;
+		if (isEmpty())
+		{
+			std::cout << "EMPTY LIST\n";
+			return;
+		}
+		for (auto itr = head; itr != nullptr; itr = itr->next)
+		{
+			std::cout << itr->data << "->";
+		}
+		std::cout << '\n';
+	}
+
+	template<typename type>
+	size_t Linked_List<type>::ssize()
+	{
+		return size;
+	}
+
+	template<typename type>
+	type& Linked_List<type>::back() const
+	{
+		return tail->data;
+	}
+
+	// Invokes UB when called on Empty list
+	template<typename type>
+	type& Linked_List<type>::front() const
+	{
+		return head->data;
+	}
+
+	// Invokes UB when called on Empty list
+	template<typename type>
+	void Linked_List<type>::clear()
+	{
+		// set Head and Tail to nullptr and reset size
+		head = tail = nullptr;
+		size = 0;
+	}
+
+	// Removes the First occurence of Value
+	template<typename type>
+	void Linked_List<type>::yeet(type value)
+	{
+		if (isEmpty())
+		{
+			std::cerr << "LIST IS EMPTY\n";
+			return;
+
+		}
+
+		// If  the value is in the head node
+		if (value == head->data)
+		{
+			pop_front();
+			return;
+		}
+
+		// If the value is in the tail node
+		if (value == tail->data)
+		{
+			pop_back(); // Use the existing pop_back() method
+			return;
+		}
+
+
+
+		auto itr = head;
+
+		// Find the the Previous Node of the Element to Remove ( Making sure it's Not null )
+		while (itr->next != nullptr && itr->next->data != value)
+		{
+			itr = itr->next;
+		}
+		// Now, itr->next points to the node that needs to be removed
+		if (itr->next == nullptr)
+		{
+			std::cerr << value << " Not Found in List\n";
+			return;
+		}
+		// Remove the node by linking to the next one
+		itr->next = itr->next->next;
+
+
+		--size;
 	}
 
 
-
-	auto itr = head;
-
-	// Find the the Previous Node of the Element to Remove ( Making sure it's Not null )
-	while (itr->next != nullptr && itr->next->data != value)
+	// Using Two-Pointer Approach Time O(N) , Space O(1)
+	template<typename type>
+	void Linked_List<type>::reverse()
 	{
-		itr = itr->next;
-	}
-	// Now, itr->next points to the node that needs to be removed
-	if (itr->next == nullptr)
-	{
-		std::cerr << value << " Not Found in List\n";
-		return;
-	}
-	// Remove the node by linking to the next one
-	itr->next = itr->next->next;
 
+		std::shared_ptr<Node> pvs = nullptr;
 
-	--size;
-}
+		auto itr{ head };
 
+		while (itr != nullptr)
+		{
+			auto temp = itr->next;
+			itr->next = pvs;
+			pvs = itr;
+			itr = temp;
 
-// Using Two-Pointer Approach Time O(N) , Space O(1)
-template<typename type>
-void Linked_List<type>::reverse()
-{
-
-	std::shared_ptr<Node> pvs = nullptr;
-
-	auto itr{ head };
-
-	while (itr != nullptr)
-	{
-		auto temp = itr->next;
-		itr->next = pvs;
-		pvs = itr;
-		itr = temp;
+		}
+		head = pvs;
 
 	}
-	head = pvs;
 
-}
-
-// Checks Whether an Value exists in List
-template<typename type>
-bool Linked_List<type>::search(const type value)const
-{
-	if (isEmpty())
+	// Checks Whether an Value exists in List
+	template<typename type>
+	bool Linked_List<type>::search(const type value)const
 	{
+		if (isEmpty())
+		{
 
+			return false;
+		}
+
+		for (auto itr = head; itr != nullptr; itr = itr->next)
+		{
+			if (itr->data == value)
+			{
+				return true;
+			}
+		}
 		return false;
 	}
 
-	for (auto itr = head; itr != nullptr; itr = itr->next)
+	template<typename type>
+	void Linked_List<type>::replace(const type value, const type newValue)
 	{
-		if (itr->data == value)
+		if (isEmpty())
 		{
-			return true;
+			std::cout << "ERROR: Attemp to call 'replace()' on empty List\n";
+			return;
+		}
+
+		for (auto itr = head; itr != nullptr; itr = itr->next)
+		{
+			if (itr->data == value)
+			{
+				itr->data = newValue;
+				break;
+			}
 		}
 	}
-	return false;
-}
 
-template<typename type>
-void Linked_List<type>::replace(const type value, const type newValue)
-{
-	if (isEmpty())
-	{
-		std::cout << "ERROR: Attemp to call 'replace()' on empty List\n";
-		return;
-	}
-
-	for (auto itr = head; itr != nullptr; itr = itr->next)
-	{
-		if (itr->data == value)
-		{
-			itr->data = newValue;
-			break;
-		}
-	}
 }
 
 
 
 
 
-
-#endif // !LINKED_LIST_HPP
+#endif // !FORWARD_LIST_HPP
 
 
--- Data_Structures/List.hpp
+++ Data_Structures/List.hpp
@@ -1,5 +1,5 @@
-#ifndef DOUBLE_LINKED_LIST_HPP
-#define DOULBE_LINKED_LIST_HPP
+#ifndef _LIST_HPP
+#define _LIST_HPP
 
 
 #include <memory>
@@ -9,130 +9,135 @@
 ///			DOUBLE LINKED LIST													///
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
-template <typename type>
-class List
+namespace xe
 {
-public:
-	List() {}
 
 
-	bool isEmpty() const;
-
-	void push_back(type value);
-
-	void push_front(type value);
-
-	void pop_back();
-
-	void display()const;
-
-private:
-	struct Node
+	template <typename type>
+	class List
 	{
-		explicit Node(type value)
-			: data{ value }, next{ nullptr }, prev{ nullptr }
-		{}
+	public:
+		List() {}
 
-		type data{};
-		std::shared_ptr<Node> next{};
-		std::shared_ptr<Node> prev{};
+
+		bool isEmpty() const;
+
+		void push_back(type value);
+
+		void push_front(type value);
+
+		void pop_back();
+
+		void display()const;
+
+	private:
+		struct Node
+		{
+			explicit Node(type value)
+				: data{ value }, next{ nullptr }, prev{ nullptr }
+			{}
+
+			type data{};
+			std::shared_ptr<Node> next{};
+			std::shared_ptr<Node> prev{};
+
+		};
+
+	private:
+
+		// Initialize Head & Tail with nullptr
+
+		std::shared_ptr<Node> head{};
+		std::shared_ptr<Node> tail{};
+
+		size_t size{};
 
 	};
-
-private:
-
-	// Initialize Head & Tail with nullptr
-
-	std::shared_ptr<Node> head{};
-	std::shared_ptr<Node> tail{};
-
-	size_t size{};
-
-};
-
-#endif // !DOUBLE_LINKED_LIST_HPP
 
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-///					TEMPLATE DEFINITION											///
-/////////////////////////////////////////////////////////////////////////////////////////////////
+///				CLASS TEMPLATE DEFINTION											///
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
+	template<typename type>
+	bool List<type>::isEmpty() const
+	{
+		return head == nullptr;
+	}
 
-template<typename type>
-bool List<type>::isEmpty() const
-{
-	return head == nullptr;
+	template<typename type>
+	void List<type>::push_back(type value)
+	{
+		auto newNode = std::make_shared<Node>(value);
+		if (isEmpty())
+		{
+			head = tail = newNode;
+		}
+		else
+		{
+			newNode->prev = tail;
+			tail->next = newNode;
+			tail = newNode;
+		}
+		++size;
+	}
+
+	template<typename type>
+	void List<type>::push_front(type value)
+	{
+		auto newNode = std::make_shared<Node>(value);
+		if (isEmpty())
+		{
+			head = tail = newNode;
+		}
+		else
+		{
+			newNode->next = head;
+			head->prev = newNode;
+			head = newNode;
+		}
+		++size;
+	}
+	template<typename type>
+	void List<type>::pop_back()
+	{
+		if (isEmpty())
+		{
+			std::cerr << "ERROR: Attempt to call 'pop_back()' on empty list\n";
+			return;
+		}
+		if (head == tail)
+		{
+			head = tail = nullptr;
+		}
+		else
+		{
+			tail = tail->prev;
+			tail->next = nullptr;
+		}
+		--size;
+	}
+
+	template<typename type>
+	void List<type>::display() const
+	{
+
+		if (isEmpty())
+		{
+			std::cout << "EMPTY LIST\n";
+			return;
+		}
+		for (auto itr = head; itr != nullptr; itr = itr->next)
+		{
+			std::cout << '-' << itr->data;
+		}
+		std::cout << "-\n";
+	}
 }
 
-template<typename type>
-void List<type>::push_back(type value)
-{
-	auto newNode = std::make_shared<Node>(value);
-	if (isEmpty())
-	{
-		head = tail = newNode;
-	}
-	else
-	{
-		newNode->prev = tail;
-		tail->next = newNode;
-		tail = newNode;
-	}
-	++size;
-}
 
-template<typename type>
-void List<type>::push_front(type value)
-{
-	auto newNode = std::make_shared<Node>(value);
-	if (isEmpty())
-	{
-		head = tail = newNode;
-	}
-	else
-	{
-		newNode->next = head;
-		head->prev = newNode;
-		head = newNode;
-	}
-	++size;
-}
-template<typename type>
-void List<type>::pop_back()
-{
-	if (isEmpty())
-	{
-		std::cerr << "ERROR: Attempt to call 'pop_back()' on empty list\n";
-		return;
-	}
-	if (head == tail)
-	{
-		head = tail = nullptr;
-	}
-	else
-	{
-		tail = tail->prev;
-		tail->next = nullptr;
-	}
-	--size;
-}
-
-template<typename type>
-void List<type>::display() const
-{
-
-	if (isEmpty())
-	{
-		std::cout << "EMPTY LIST\n";
-		return;
-	}
-	for (auto itr = head; itr != nullptr; itr = itr->next)
-	{
-		std::cout << '-' << itr->data;
-	}
-	std::cout << "-\n";
-}
+#endif
 
 
 
